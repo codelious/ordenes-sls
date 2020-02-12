c0ae2ec24b53b50b81a153ec69f8bfd30ca4946c
Mario Espinoza <marioespinozaaguayo@gmail.com>
2020-02-11 23:40:49 -0300

    creando orden

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const uuidv1 = require('uuid/v1')
+
 module.exports.hello = async event => {
   return {
     statusCode: 200,
@@ -16,9 +18,24 @@ module.exports.hello = async event => {
 
 
 module.exports.nuevaOrden = function(event, context, callback) {
-  https.get(url, (res) => {
-    callback(null, res.statusCode)
-  }).on('error', (e) => {
-    callback(Error(e))
-  })
+  
+  console.log('recibiendo una nueva orden');
+  const body = JSON.parse(event.body);
+
+  const orden = {
+    id: uuidv1(),
+    tipo: body.tipo,
+    descripcion: body.descripcion,
+    timestamp: new Date()
+  }
+
+  sendResponse(200, orden, callback);
+}
+
+function sendResponse(statusCode, message, callback) {
+  const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	callback(null, response);
 }
